0a414bc9f85289b65f61cccf34b46107fdd96947
arshiya <arshiyakhalili6@gmail.com>
2025-08-18 16:52:50 +0330

    chore: improve databse intializer

--- pkg/db/database.go
+++ pkg/db/database.go
@@ -10,30 +10,35 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var DB *sql.DB
-
-func Init() error {
-
+func Init() (*sql.DB, error) {
+	// Load env file if present
 	if err := godotenv.Load(); err != nil {
-		log.Println("NO .env gile found")
+		log.Println("No .env file found")
 	} else {
-		log.Println("Loaded enviroment vriables from .env")
+		log.Println("Loaded environment variables from .env")
 	}
 
+	// Build DSN
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"))
+		os.Getenv("DB_NAME"),
+	)
 
-	fmt.Println(dsn)
-	var err error
-
-	DB, err = sql.Open("postgres", dsn)
+	// Open DB connection
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return DB.Ping()
+
+	// Test the connection
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to database successfully")
+	return db, nil
 }
